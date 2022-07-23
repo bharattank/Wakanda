226aa3c99d32cc7a5f25fcc0c8db99d9579b8b85
Bharat Tank <bharattank903@gmail.com>
2022-07-23 16:13:40 +0530

    Make Blog page 3 columns

--- index.php
+++ index.php
@@ -12,12 +12,42 @@ get_header();
             if( have_posts() ) {
                 ?>
                 <div class="container">
-                    <?php
-                    while ( have_posts() ) : the_post();
-                    the_title();
-                    the_content();
-                    endwhile;
+                    <?php 
+                    if ( is_home() && ! is_front_page() ) {
+                        ?>
+                        <header class="mb-5">
+                            <h1 class="page-title">
+                                <?php echo single_post_title(); ?>
+                            </h1>
+                        </header>
+                        <?php
+                    }
                     ?>
+                    <div class="row">
+                    <?php
+                    $index = 0;
+                    $no_of_columns = 3;
+                        while ( have_posts() ) : the_post();
+                        if ( 0 === $index % $no_of_columns ) {
+                            ?>
+                            <div class="col-lg-4 col-md-6 col-12">
+                            <?php
+                        }
+                        ?>
+                        <h3><?php the_title(); ?></h3>
+                        <div><?php the_excerpt(); ?></div>
+                        <?php
+
+                        $index ++;
+
+                        if ( 0 !== $index && 0 === $index % $no_of_columns ) {
+                            ?>
+                            </div>
+                            <?php
+                        }
+                        endwhile;
+                    ?>
+                    </div>
                 </div>
                 <?php
             }
